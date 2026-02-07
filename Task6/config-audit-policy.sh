f90f51e907dc72d8e3ee52a77e85ebbd23d92a44
Baratashvili Vadim <bar_vad@list.ru>
2026-02-08 02:01:12 +0300

    fix

--- Task6/config-audit-policy.sh
+++ Task6/config-audit-policy.sh
@@ -7,7 +7,7 @@ minikube start --force \
   --driver=docker \
   --container-runtime=docker \
   --kubernetes-version=v1.29.4 \
-  --extra-config=kubelet.cgroup-driver=cgroupfs \ 
+  --extra-config=kubelet.cgroup-driver=cgroupfs \
   --mount \
   --mount-string="$HOME/k8s-audit:/etc/kubernetes/audit" \
   --extra-config=apiserver.audit-policy-file=/etc/kubernetes/audit/audit-policy.yaml \
