1c75298ddd70136e983108217cfa312a14c53990
Baratashvili Vadim <bar_vad@list.ru>
2026-02-08 01:28:16 +0300

    config-audit-policy.sh

--- Task6/config-audit-policy.sh
+++ Task6/config-audit-policy.sh
@@ -4,11 +4,6 @@ mkdir -p ~/k8s-audit && cp audit-policy.yaml ~/k8s-audit/
 cd ~/k8s-audit
 
 minikube start \
-  --driver=docker \
-  --container-runtime=docker \
-  --force-systemd=true \
-  --kubernetes-version=v1.29.4 \
-  --extra-config=kubelet.cgroup-driver=cgroupfs \
   --mount \
   --mount-string="$HOME/k8s-audit:/etc/kubernetes/audit" \
   --extra-config=apiserver.audit-policy-file=/etc/kubernetes/audit/audit-policy.yaml \
