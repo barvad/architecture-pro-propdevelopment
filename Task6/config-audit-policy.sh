07f8ce3719929b9ab79c06fdd1d4dbb0086cb13c
Baratashvili Vadim <bar_vad@list.ru>
2026-02-08 02:13:36 +0300

    fix dir

--- Task6/config-audit-policy.sh
+++ Task6/config-audit-policy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-mkdir -p ~/k8s-audit && cp audit-policy.yaml ~/k8s-audit/
-cd ~/k8s-audit
-
 minikube start --force \
   --driver=docker \
   --container-runtime=docker \
   --kubernetes-version=v1.29.4 \
   --extra-config=kubelet.cgroup-driver=cgroupfs \
   --mount \
-  --mount-string="$HOME/k8s-audit:/etc/kubernetes/audit" \
+  --mount-string="$(pwd):/etc/kubernetes/audit" \
   --extra-config=apiserver.audit-policy-file=/etc/kubernetes/audit/audit-policy.yaml \
-  --extra-config=apiserver.audit-log-path=/var/log/audit.log \
+  --extra-config=apiserver.audit-log-path=/etc/kubernetes/audit/udit.log \
   --extra-config=apiserver.audit-log-maxage=7 \
   --extra-config=apiserver.audit-log-maxbackup=10 \
   --extra-config=apiserver.audit-log-maxsize=100
