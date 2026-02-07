d5bb56758f39a0d5ad780b5bb13d3d462ebbc638
Baratashvili Vadim <bar_vad@list.ru>
2026-02-08 02:20:44 +0300

    qemu

--- Task6/config-audit-policy.sh
+++ Task6/config-audit-policy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 minikube start --force \
-  --driver=docker \
+  --driver=qemu \
   --container-runtime=docker \
   --kubernetes-version=v1.29.4 \
   --extra-config=kubelet.cgroup-driver=cgroupfs \
